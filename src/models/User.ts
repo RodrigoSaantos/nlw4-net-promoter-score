b6f618688cb33a6eea00144ddc3f21d65da8cc0f
rodrigosaantos <contato.rodrigosaantos@gmail.com>
2021-03-14 20:20:44 -0300

    Creating models

---
+++ src/models/User.ts
@@ -0,0 +1,29 @@
+import { v4 as uuid } from 'uuid';
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
+
+@Entity("Users")
+class User {
+
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if(!this.id) {
+      this.id = uuid()
+    }
+  }
+}
+
+export { User }
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import 'reflect-metadata'
-import express from 'express';
 import './database';
+import express from 'express';
+import { router } from './routes';
 
 const app = express();
+app.use(express.json())
 
-
-app.get("/users", (request, response) => {
-  return response.json({ message: 'Hello world'})
-})
+app.use(router)
 
 app.listen(3333, () => {
   console.log('Server is running 🚀');

85b4bb0568c55ab9ce9abe4be44d2e6095eaef31
rodrigosaantos <contato.rodrigosaantos@gmail.com>
2021-03-15 20:50:14 -0300

    Creating models survey

---
+++ src/models/Survey.ts
@@ -0,0 +1,30 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import { v4 as uuid } from 'uuid';
+
+@Entity('Surveys')
+class Survey {
+
+  @PrimaryColumn()
+  readonly id: string
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if(!this.id) {
+      this.id = uuid();
+    }
+  }
+  
+}
+
+export { Survey };

f6ba0554c0382372e01efe2f9a05f1b4bd5043ed
rodrigosaantos <contato.rodrigosaantos@gmail.com>
2021-03-15 20:34:05 -0300

    Creating repository

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,13 +1,28 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
 
 class UserController {
   async create(request: Request, response: Response) {
-    const body = request.body
+    const { name, email } = request.body
 
-    console.log(body);
+    const usersRepository = getCustomRepository(UsersRepository);
 
-    return response.send();
+    const userAlredyExists = await usersRepository.findOne({ email });
+
+    if (userAlredyExists) {
+      return response.status(400).json({ Error: 'User alredy exists!'})
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+    });
+
+    await usersRepository.save(user);
+
+    return response.json(user);
   }
 }
 
-export { UserController }
+export { UserController };
